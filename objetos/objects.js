4e0f2356907232fd24c957c528902224ab244001
Fernando Nicolás <fersanti2896@gmail.com>
2022-07-21 23:47:40 -0500

    Objetos basicos en objects.ts

---
+++ objetos/objects.js
@@ -0,0 +1,14 @@
+"use strict";
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super Velocidad', 'Viajar en el tiempo']
+    };
+    flash = {
+        name: 'Clark Kent',
+        age: 45,
+        powers: ['Super Fuerza', 'Visión aumentada']
+    };
+    console.log({ flash });
+})();
